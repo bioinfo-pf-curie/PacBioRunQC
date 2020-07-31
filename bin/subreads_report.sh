57c0ba846365bd2192beb8e867092a7df91b2554
fallain <fabrice.allain@curie.fr>
2020-07-31 18:05:21 +0200

    [MODIF] add pacbiosmrtlink.def recipe (issue #1)

--- bin/subreads_report.sh
+++ bin/subreads_report.sh
@@ -6,7 +6,7 @@ name=$3
 
 ##python_2.7.16 
 
-/bioinfo/local/build/pacbio-smrtlink/app_v4.0/smrtcmds/bin/python /bioinfo/local/build/pacbio-smrtlink/app_v4.0/install/smrtlink-release_7.0.1.66975/bundles/smrttools/install/smrttools-release_7.0.1.66768/private/pacbio/pythonpkgs/pbreports/lib/python2.7/site-packages/pbreports/report/subreads_reports.py $2 $1_subreads_reports.json
+subreads_reports.py $2 $1_subreads_reports.json
 
 mkdir $3
 mv loading_xml/ $3
