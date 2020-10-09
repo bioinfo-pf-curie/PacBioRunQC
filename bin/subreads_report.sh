9d75a977292009db2be8701973a18e12b507aada
Fabrice Allain <fabrice.allain@curie.fr>
2020-10-09 11:36:14 +0200

    [MODIF] remove absolute paths in subreads links (#8)

--- bin/subreads_report.sh
+++ bin/subreads_report.sh
@@ -3,32 +3,29 @@
 reads=$1
 name=$2
 
-##python_2.7.16 
-
-#subreads_reports.py $2 $1_subreads_reports.json
 
 #########
 mkdir adapter_xml
-/bioinfo/local/build/pacbio-smrtlink/app_v4.0/smrtcmds/bin/python3 -m pbreports.report.adapter_xml --log-level INFO --optional $1 adapter_xml.json
+python3 -m pbreports.report.adapter_xml --log-level INFO --optional $1 adapter_xml.json
 mv adapter_xml.json adapter_xml
 mv interAdapterDist0.* adapter_xml
 
 #########
 mkdir control
-/bioinfo/local/build/pacbio-smrtlink/app_v4.0/smrtcmds/bin/python3 -m pbreports.report.control --log-level INFO --optional $1 control.json
+python3 -m pbreports.report.control --log-level INFO --optional $1 control.json
 mv control.json control
 mv readlength_plot.* control
 mv concordance_plot.* control
 
 #########
 mkdir loading_xml
-/bioinfo/local/build/pacbio-smrtlink/app_v4.0/smrtcmds/bin/python3 -m pbreports.report.loading_xml --log-level INFO --optional $1 loading_xml.json
+python3 -m pbreports.report.loading_xml --log-level INFO --optional $1 loading_xml.json
 mv loading_xml.json loading_xml
 mv raw_read_length_plot.* loading_xml
 
 #########
 mkdir filter_stats_xml
-/bioinfo/local/build/pacbio-smrtlink/app_v4.0/smrtcmds/bin/python3 -m pbreports.report.filter_stats_xml --log-level INFO --optional $1 filter_stats_xml.json
+python3 -m pbreports.report.filter_stats_xml --log-level INFO --optional $1 filter_stats_xml.json
 mv filter_stats_xml.json filter_stats_xml
 mv readLenDist0.* filter_stats_xml
 mv subread_lengths.* filter_stats_xml
